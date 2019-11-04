79c1f4e5a98558d22534341a902e208be839853c
Taylor <tcour10@gmail.com>
2019-11-04 13:27:13 -0600

    Added validations to Product and Category

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
+# frozen_string_literal: true
+
 class Product < ApplicationRecord
   has_many :product_categories, dependent: :destroy
   has_many :categories, through: :product_categories
+
+  validates :name, :description, :price, presence: true
+  validates :price, numericality: { only_integer: true }
 end
